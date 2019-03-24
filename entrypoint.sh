b3d7b0054dfd79b20a0d4dae60be43defbc1b879
Christian Emmer <christianemmer@stockx.com>
2019-03-24 16:12:57 -0400

    Initial commit.

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -euo pipefail
+
+
+# Keep FlexGet updated
+pip3 install --upgrade flexget==${VERSION}
+
+# Default FlexGet config
+if [[ ! -f /config/config.yml ]]; then
+    cp /flexget.yml /config/config.yml
+fi
+
+flexget daemon start --autoreload-config
