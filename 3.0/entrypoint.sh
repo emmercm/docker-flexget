e625548871517583a946d2691f5be49d85aa3a3f
Christian Emmer <emmercm@gmail.com>
2019-11-24 18:18:23 -0500

    No relative symlinks for Docker builds.

--- 3.0/entrypoint.sh
+++ 3.0/entrypoint.sh
@@ -1,1 +1,23 @@
-../entrypoint.sh
+#!/usr/bin/env sh
+set -euo pipefail
+
+
+# Remove config lock
+if [[ -f /config/.config-lock ]]; then
+    rm /config/.config-lock
+fi
+
+# Keep FlexGet updated
+echo "Checking for FlexGet updates ..."
+pip${PYTHON_VERSION/2/} install --upgrade --quiet pip
+pip${PYTHON_VERSION/2/} install --upgrade --quiet flexget~=${FLEXGET_VERSION}
+
+# Default FlexGet config
+if [[ ! -f /config/config.yml ]]; then
+    cp /flexget.yml /config/config.yml
+fi
+
+# Set web UI password
+flexget web passwd ${FLEXGET_PASSWORD}
+
+exec "$@"
