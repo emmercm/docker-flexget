8d2e8e3232b88e3f2c07e85fd436501d44537568
Christian Emmer <emmercm@gmail.com>
2019-12-26 11:55:05 -0500

    Don't keep old major versions updated.

--- 2.21/entrypoint.sh
+++ 2.21/entrypoint.sh
@@ -7,11 +7,6 @@ if [[ -f /config/.config-lock ]]; then
     rm /config/.config-lock
 fi
 
-# Keep FlexGet updated
-echo "Checking for FlexGet updates ..."
-pip${PYTHON_VERSION/2/} install --upgrade --quiet pip
-pip${PYTHON_VERSION/2/} install --upgrade --quiet flexget~=${FLEXGET_VERSION}
-
 # Python 3 fix
 find /usr/lib/python3*/site-packages/flexget -type f -name "*" -print0 | xargs -0 sed -i 's/future.moves.urllib/urllib/g'
 
