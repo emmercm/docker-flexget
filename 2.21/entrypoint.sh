d8c3a77ce92a995d0613eae9294817490043fa3a
Christian Emmer <emmercm@gmail.com>
2019-12-26 11:55:14 -0500

    Move Python fix to Dockerfile.

--- 2.21/entrypoint.sh
+++ 2.21/entrypoint.sh
@@ -7,9 +7,6 @@ if [[ -f /config/.config-lock ]]; then
     rm /config/.config-lock
 fi
 
-# Python 3 fix
-find /usr/lib/python3*/site-packages/flexget -type f -name "*" -print0 | xargs -0 sed -i 's/future.moves.urllib/urllib/g'
-
 # Default FlexGet config
 if [[ ! -f /config/config.yml ]]; then
     cp /flexget.yml /config/config.yml
